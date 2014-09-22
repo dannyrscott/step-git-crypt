2f639bbbd120c74fd4c95af13287db5b8f136169
dannyrscott <dscott@leadnomics.com>
2014-09-22 10:33:38 -0400

    use git-crypt 0.3

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git clone git@github.com:AGWA/git-crypt.git /pipeline/git-crypt
-cd /pipeline/git-crypt; sudo make; sudo cp git-crypt /usr/local/bin/
+cd /pipeline/git-crypt; git checkout 0.3; sudo make; sudo cp git-crypt /usr/local/bin/
